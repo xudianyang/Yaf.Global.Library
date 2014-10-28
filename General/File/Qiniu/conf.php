f691e8959d55a5c5521fc0f1bb16bacb30cff6e9
xudianyang <xudianyang@phpboy.net>
2014-10-28 11:58:43 +0800

    * 恢复.me域名

--- General/File/Qiniu/conf.php
+++ General/File/Qiniu/conf.php
@@ -11,8 +11,8 @@ global $QINIU_SECRET_KEY;
 $SDK_VER = "6.1.9";
 
 $QINIU_UP_HOST	= 'http://up.qiniu.com';
-$QINIU_RS_HOST	= 'http://rs.qbox.com';
-$QINIU_RSF_HOST	= 'http://rsf.qbox.com';
+$QINIU_RS_HOST	= 'http://rs.qbox.me';
+$QINIU_RSF_HOST	= 'http://rsf.qbox.me';
 
 $QINIU_ACCESS_KEY	= '<Please apply your access key>';
 $QINIU_SECRET_KEY	= '<Dont send your secret key to anyone>';
